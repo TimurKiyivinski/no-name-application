a7fafc53ae967c2065b3533fc8cfa3c48078130e
TimurKiyivinski <deathseclipse@gmail.com>
2014-10-22 12:36:13 +0800

    XML

--- task_scheduler.py
+++ task_scheduler.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python2
+import xml.etree.ElementTree as ET
 
 DATABASE = 'schedule.db'
 EMPTY = ''
@@ -6,21 +7,44 @@ SPACE = ' '
 
 #Task Class
 class task:
-    def __init__(self, newStatus, newCategory, newExplain, newWeek, newDay):
+    def __init__(self,
+            newCat = "",
+            newName = "",
+            newStatus = "",
+            newExplain = "",
+            newWeek = 0,
+            newDay = 0):
+        self.category = newCat
+        self.name = newName
         self.status = newStatus
-        self.category = newCategory
         self.explain = newExplain
         self.week = newWeek
         self.day = newDay
+    def setCategory(self, newCategory):
+        self.category = newCategory
+    def setStatus(self, newStatus):
+        self.status = newStatus
+    def setName(self, newName):
+        self.name = newName
+    def setExplain(self, newExplain):
+        self.explain = newExplain
+    def setWeek(self, newWeek):
+        if newWeek in range(1, 53):
+            self.week = newWeek
+        else:
+            self.week = -1
+    def setDay(self, newDay):
+        if newDay in range(0, 7):
+            self.day = newDay
+        else:
+            self.day = -1
     def toString(self):
-        returnString = EMPTY
-        if self.status != EMPTY:
-            returnString = wrapString('[', self.status) + SPACE
-        returnString += wrapString('{', self.category) + SPACE + self.explain
+        returnString = wrapString(self.status, '[') + SPACE
+        returnString += wrapString(self.category, '{') + SPACE + self.explain
         return returnString
 
 #Wraps a string
-def wrapString(wrapper, string):
+def wrapString(string, wrapper):
     wrapEnd = ''
     if wrapper == '[':
         wrapEnd = ']'
@@ -40,19 +64,37 @@ def dbSetup():
 
 #Reads the user's schedule from the database
 def getUserSchedule():
-    schedule_1 = task('DONE', 'PERSONAL', 'Get this message to send.', 2, 0)
-    schedule_2 = task(EMPTY, 'WORK', 'Submit reports.', 2, 0)
-    schedule_3 = task(EMPTY, 'PERSONAL', 'Write a biography on my sad life.', 2, 0)
-    schedule_4 = task('DONE', 'PERSONAL', 'Get this message to send again.', 2, 0)
-    schedule_5 = task('POSTPONED', 'WORK', 'Life threatening mission.', 2, 0)
-    schedule_6 = task(EMPTY, 'PERSONAL', 'Dwell on the meaning on keyboards.', 2, 0)
-    schedules = [schedule_1, schedule_2, schedule_3, schedule_4, schedule_5, schedule_6]
+    schedules = []
+    tree = ET.parse('Schedules/Games.xml')
+    root_xml = tree.getroot()
+    for week_xml in root_xml:
+        for task_xml in week_xml:
+            new_task = task()
+            new_task.setCategory("Games")
+            for items_xml in task_xml:
+                items_tag = items_xml.tag
+                items_text = items_xml.text
+                if items_text is None:
+                    continue
+                if items_tag == 'status':
+                    new_task.setStatus(items_text)
+                elif items_tag == 'name':
+                    new_task.setName(items_text)
+                elif items_tag == 'desc':
+                    new_task.setExplain(items_text)
+                elif items_tag == 'day':
+                    new_task.setDay(items_text)
+                else:
+                    print "Unknown tag:"
+                    print items_tag
+            schedules.append(new_task)
     return schedules
 
 #Main function for CLI usage
 def main(userSchedule):
     for schedule in userSchedule:
         print schedule.toString()
+    '''
     userInput = EMPTY
     while True:
         print 'Task Scheduler'
@@ -71,6 +113,7 @@ def main(userSchedule):
             quit('Bye!')
         else:
             print 'Invalid selection!'
+        '''
 
 #Run the application
 if __name__ == '__main__':
